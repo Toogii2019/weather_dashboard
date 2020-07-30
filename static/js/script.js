747e77763504d5abda211f9eb0bfcb0faf1d828b
Toogii <dashdava@uw.edu>
2020-07-30 09:58:57 -0700

    Removing key

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 $("document").ready(function() {
-  var APIKey = "166a433c57516f51dfab1f7edaed8413";
+  var APIKey = "API_KEY";
     // Here we are building the URL we need to query the database
     // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Bujumbura,Burundi&appid=" + APIKey;
     // Here we run our AJAX call to the OpenWeatherMap API
